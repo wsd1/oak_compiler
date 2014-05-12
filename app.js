ea2ed5d70b99000a7c74496e68a730ecb452b573
丁一 <ws_dy@sina.com>
2014-05-12 22:54:04 +0800

    add upload

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var upload = require('./routes/upload');
 //console.log("type:" + typeof routes);
 
 var app = express();
