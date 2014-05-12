f77702488d00f7433c7b621772bfe611a8f1f5f3
root <root@dyvps.(none)>
2014-05-12 08:48:26 -0400

    init

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+//console.log("type:" + typeof router.get);
+
+module.exports = router;
